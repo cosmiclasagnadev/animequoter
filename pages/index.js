d8ae45ecfa1b869cd477ad549a21ab7b7ba1545b
Allen Ponce de Leon <allen@DESKTOP-90GUCIQ.localdomain>
2021-07-26 14:09:31 +0800

    Initialized git repo

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,69 @@
 import Head from 'next/head'
+import { useState } from "react";
+const axios = require('axios').default;
+
+const getRandomQuote = async () => {
+  const data = await axios.get('https://animechan.vercel.app/api/random');
+  // console.log(data.data);
+  return data.data
+}
+
+var animeImageOptions  = {
+  method: 'GET',
+  url: 'https://bing-image-search1.p.rapidapi.com/images/search',
+  params: {q: 'anime', count: '1'},
+  headers: {
+    'x-rapidapi-key': '67b335ab79mshc760d0fdd372facp1301e2jsn8d7acb97a43b',
+    'x-rapidapi-host': 'bing-image-search1.p.rapidapi.com'
+  }
+};
+
+const getAnimeImage= async () => {
+  const imageData = await axios.request(animeImageOptions)
+  console.log(imageData.data.value[0].contentUrl)
+  const imageDataUrl = imageData.data.value[0].contentUrl
+  return imageDataUrl;
+
+}
+
+
 
 export default function Home() {
+  const [animeQuote, setAnimeQuote] = useState('')
+  const [imageUrl, setImageUrl] = useState('https://i.stack.imgur.com/y9DpT.jpg')
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-indigo-800">
       <Head>
-        <title>Create Next App</title>
+        <title>Anime Quoter 🍣</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+        <h1 className="text-6xl font-bold text-blue-200 py-7">
+          Anime Quoter
         </h1>
+        <button onClick={async () => {
+          const animeData = await getRandomQuote();
+          // console.log(animeData)
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+          const { quote } = animeData;
+          // console.log(quote)
+          setAnimeQuote(quote)
+          const dataUrl = await getAnimeImage();
+          setImageUrl(dataUrl)
+        }} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Generate a Random Quote! 🍣
+        </button>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div class="py-4">
+          <img class="rounded-sm object-contain h-48 w-full" src={imageUrl}/>
         </div>
+        <div>
+          <p>{animeQuote}</p>
+        </div>
+        
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
 }
